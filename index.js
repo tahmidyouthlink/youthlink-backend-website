2d138f41927fb51f8d09afd947992f9fa770430f
tahmidyouthlink <t.rahman@youthlink.tech>
2024-05-13 19:45:35 +0600

    added post delete method for job circular

--- index.js
+++ index.js
@@ -89,14 +89,6 @@ async function run() {
             res.send(result);
         })
 
-        // delete
-        app.delete("/deleteBlogPerArray/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await blogCollection.deleteOne(query);
-            res.send(result);
-        })
-
         // get single blog info
         app.get("/allBlog/:id", async (req, res) => {
             const id = req.params.id;
